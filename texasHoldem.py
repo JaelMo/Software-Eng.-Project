9b295446ac80334a67dc22a5c2cb4ffaccaaf80f
kromephist <93674088+kromephist@users.noreply.github.com>
2021-11-05 20:06:42 -0500

    Update texasHoldem.py

--- texasHoldem.py
+++ texasHoldem.py
@@ -11,7 +11,7 @@ ranks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 suits = ["Clubs","Diamonds","Hearts","Spades"]
 
 player_hand = []
-community_cards = []
+
 aIhand = []
 
 def  card_define():
@@ -24,16 +24,27 @@ def  card_define():
 def dealPlayer():
     for count in range(0,2):
         player_hand.append(card_define())
+    if player_hand[0] == player_hand[1]:
+        player_hand[1] = card_define()
+    for count in range(0,2):   
         print(player_hand[count].rank, player_hand[count].suit )
 
 def dealAI():
     for count in range(0,2):
         aIhand.append(card_define())
+    if aIhand[0] == aIhand[1]:
+        aIhand[1] = card_define()
+    for count in range(0,2): 
         print(aIhand[count].rank, aIhand[count].suit)
+
+community_cards = []
 
 def dealCommunity():
     for count in range(0,5):
         community_cards.append(card_define())
+        for count2 in range(0,len(community_cards)-1):
+                if community_cards[count2] == community_cards[count]:
+                    community_cards[count2] = card_define()
         print(community_cards[count].rank, community_cards[count].suit )
 
 
@@ -47,14 +58,28 @@ print("----------")
 def compare():
     playerscore = 0
     for count in range(0,5):
-        if player_hand[0].rank == community_cards[count].rank:
-            if player_hand[0].suit == community_cards[count].suit:
-                playerscore = playerscore + 1
-        if player_hand[1].rank == community_cards[count].rank:
-            if player_hand[1].suit == community_cards[count].suit:
-                playerscore = playerscore + 1
-        if community_cards[count].rank == community_cards[].rank:
-            playerscore=playerscore+1
-    print(playerscore)
-compare()
 
+        if player_hand[0].rank == community_cards[count].rank:
+            if player_hand[0].suit != community_cards[count].suit:
+                playerscore += 1
+        if player_hand[1].rank == community_cards[count].rank:
+            if player_hand[1].suit != community_cards[count].suit:
+                playerscore += 1
+        for count2 in range (0,count): 
+            if count != count2:
+                if community_cards[count].rank == community_cards[count2].rank:
+                    playerscore += 1
+    if player_hand[0].rank ==  player_hand[1].rank:
+            playerscore += 1 
+    print(playerscore)
+
+
+
+
+#compare()
+from collections import Counter
+def compare2():
+    a = dict(Counter(community_cards))
+    print(a)
+
+compare2()
